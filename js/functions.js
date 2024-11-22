88f0fc54e039d3d80ee639b22e520188624366ba
animuyr <ryumina194@gmail.com>
2024-11-22 18:58:07 +0700

    Исправила функции

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-unused-vars */
 
-function strLen(str, len) {
+function checkStrLen(str, len) {
   return str.length <= len;
 }
 
-// console.log(strLen('строка строка строка строка', 20));
-// console.log(str_len('строка', 3));
-// console.log(str_len('строка', 15));
+// console.log(checkStrLen('строка строка строка строка', 20));
+// console.log(checkStrLen('строка', 3));
+// console.log(checkStrLen('строка', 15));
 
 function isPalindrome(str) {
-  str = str.toLowerCase().replaceAll(' ', '');
-  return str === str.split('').reverse().join('');
+  const normalizedStr = str.toLowerCase().replaceAll(" ", "");
+  return normalizedStr === normalizedStr.split("").reverse().join("");
 }
 
 // console.log(isPalindrome('Лёша на полке клопа нашёл '));
@@ -19,22 +19,22 @@ function isPalindrome(str) {
 // console.log(isPalindrome('Кекс'));
 
 
-function toNum (str) {
-  str = str.toString();
-  let newNum = '';
+function toNum(inputValue) {
+  const str = inputValue.toString();
+  let newNum = "";
   for (let i = 0; i < str.length; i++) {
-    if (!isNaN(parseInt(str[i], 10)) && !(str[i] === '0' && newNum === '')) {
+    if (!isNaN(parseInt(str[i], 10))) {
       newNum += str[i];
     }
   }
-  return newNum !== '' ? newNum : NaN;
+  return newNum !== "" ? parseInt(newNum, 10) : NaN;
 }
 
-// console.log(to_Num('2023 год'));
-// console.log(to_Num('ECMAScript 2022'));
-// console.log(to_Num('1 кефир, 0.5 батона'));
-// console.log(to_Num('агент 007'));
-// console.log(to_Num('2023 год'));
-// console.log(to_Num(2023));
-// console.log(to_Num(-1));
-// console.log(to_Num(1.5))
+//console.log(toNum('2023 год'));
+// console.log(toNum('ECMAScript 2022'));
+// console.log(toNum('1 кефир, 0.5 батона'));
+// console.log(toNum('агент 007'));
+// console.log(toNum('2023 год'));
+// console.log(toNum(2023));
+// console.log(toNum(-1));
+// console.log(toNum(1.5))
